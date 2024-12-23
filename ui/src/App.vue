de8ceca9fa8ea79fe9715a6a0057689f571babee
Vincent <vincent.westenberg@posteo.de>
2024-12-23 16:58:20 +0100

    Add app bar with user menu

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,7 +1,33 @@
 <template>
   <v-app>
     <v-main class="pa-3">
-      <number-generator />
+      <v-app-bar color="primary">
+        <template #append>
+          <v-menu>
+            <template #activator="{ props }">
+              <v-btn
+                icon="mdi-account-circle"
+                v-bind="props"
+              />
+            </template>
+            <v-list density="compact">
+              <v-list-item
+                prepend-icon="mdi-wrench"
+                title="Administration"
+                @click="console.log('settings')"
+              />
+              <v-list-item
+                prepend-icon="mdi-logout"
+                title="Logout"
+                @click="console.log('logout')"
+              />
+            </v-list>
+          </v-menu>
+        </template>
+      </v-app-bar>
+      <v-main>
+        <number-generator />
+      </v-main>
     </v-main>
   </v-app>
 </template>
