8e78c22d56bc474ea8415ed68c3de018674c43e7
Vincent <vincent.westenberg@posteo.de>
2024-12-23 06:50:34 +0100

    Add NumberGenerator component

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <v-app>
-    <v-main>
-      Numgen
+    <v-main class="pa-3">
+      <number-generator />
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
+import NumberGenerator from '@/components/NumberGenerator.vue';
 </script>
---
+++ ui/src/components/NumberGenerator.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card class="d-flex flex-column pa-3">
+    <v-card-title>Number Generator</v-card-title>
+    <v-card-text>
+      <v-card class="pa-3 text-h3 text-mono text-center" variant="outlined">
+        XXX-XXXXX-XXXXX-XXXXX-XXXXX
+      </v-card>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        class="text-none"
+        color="primary"
+        prepend-icon="mdi-counter"
+        variant="flat"
+      >
+        Generate
+      </v-btn>
+      <v-btn
+        class="text-none"
+        color="secondary"
+        prepend-icon="mdi-content-copy"
+        variant="flat"
+      >
+        Copy
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+</script>
