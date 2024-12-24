d2a65319d35deb96fc4d0917a8b9105e3153df31
Vincent <vincent.westenberg@posteo.de>
2024-12-24 06:29:51 +0100

    Use Vue Router

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -10,11 +10,21 @@ import '@mdi/font/css/materialdesignicons.css';
 
 const app = createApp(App);
 
+import { createRouter, createWebHistory } from 'vue-router';
+import routes from '@/router/routes';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+});
+
 const vuetify = createVuetify({
   theme: {
     defaultTheme: 'light',
   },
 });
+
+app.use(router);
 
 app.use(vuetify);
 
---
+++ ui/src/pages/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <number-generator />
+</template>
+
+<script setup lang="ts">
+import NumberGenerator from '@/components/NumberGenerator.vue';
+</script>
